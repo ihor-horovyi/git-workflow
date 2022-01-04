64b523b08daae2b9ae6d9ef5cc44b5d80097ddb0
ihor.horovyi <ihor.horovyi@vitechteam.com>
2022-01-04 17:01:44 +0100

    test-5: added new class

--- src/java/Test.java
+++ src/java/Test.java
@@ -4,5 +4,9 @@ public class Test {
   public static void main(String[] args) {
     System.out.println("Hello");
     System.out.println("11111");
+
+    var igor = new User(1, "Igor");
+    System.out.println(igor.getId());
+    System.out.println(igor.getName());
   }
 }
