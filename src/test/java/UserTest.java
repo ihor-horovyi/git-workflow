52378c8a46121c20adca6de2acc760ee08040a9e
ihor.horovyi <ihor.horovyi@vitechteam.com>
2022-01-04 17:44:32 +0100

    test-8: refactored print method

---
+++ src/test/java/UserTest.java
@@ -0,0 +1,18 @@
+package src.test.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UserTest {
+
+  @Test
+  public void getId() {
+    assertEquals("test", "test");
+  }
+
+  @Test
+  public void getName() {
+    assertEquals(1, 1);
+  }
+}
